877feecb848d734e238da0892c3a1dc1c60d5e20
Alexander Bezzubov <bzz@apache.org>
2017-12-07 21:50:05 +0100

    Refactor tests to use shared session
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- src/test/scala/tech/sourced/gemini/CassandraSparkSpec.scala
+++ src/test/scala/tech/sourced/gemini/CassandraSparkSpec.scala
@@ -1,9 +1,10 @@
 package tech.sourced.gemini
 
+import com.datastax.driver.core.Session
 import com.datastax.spark.connector.cql.CassandraConnector
 import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
@@ -11,13 +12,16 @@ import scala.concurrent.{Await, Future}
 class CassandraSparkSpec extends FlatSpec
   with Matchers
   with BaseSparkSpec
-  with Logging {
+  with Logging
+  with BeforeAndAfterAll {
 
   //to start Embedded Cassandra:
   // with SparkTemplate with EmbeddedCassandra
   // useCassandraConfig(Seq(YamlTransformations.Default))
   // override def clearCache(): Unit = CassandraConnector.evictCache()
   // + spark-cassandra-connector/blob/master/spark-cassandra-connector/src/it/resources/cassandra-3.2.yaml.template
+
+  var session: Session = _
 
   val defaultConf = new SparkConf(true)
     .set("spark.cassandra.connection.host", Gemini.defaultCassandraHost)
@@ -30,16 +34,20 @@ class CassandraSparkSpec extends FlatSpec
 
   useSparkConf(defaultConf)
 
-  val conn = CassandraConnector(defaultConf)
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    session = CassandraConnector(defaultConf).openSession()
+    awaitAll(Gemini.applySchema(session, "src/main/resources/schema.cql"))
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    session.close()
+  }
 
   def awaitAll(units: TraversableOnce[Future[Any]]): Unit = {
     implicit val ec = scala.concurrent.ExecutionContext.global
     Await.result(Future.sequence(units), Duration.Inf)
-  }
-
-  //create schema
-  conn.withSessionDo { session =>
-    awaitAll(Gemini.applySchema(session, "src/main/resources/schema.cql"))
   }
 
   "Read from Cassandra" should "return same results as were written" in {
@@ -48,10 +56,8 @@ class CassandraSparkSpec extends FlatSpec
     //TODO(bzz): repo URL list, that will be fetched by Engine
     gemini.hash("src/test/resources/siva")
 
-    conn.withSessionDo { session =>
-      val sha1 = Gemini.query("LICENSE", session)
-      sha1.head.sha should be("097f4a292c384e002c5b5ce8e15d746849af7b37") // git hash-object -w LICENSE
-    }
+    val sha1 = Gemini.query("LICENSE", session)
+    sha1.head.sha should be("097f4a292c384e002c5b5ce8e15d746849af7b37") // git hash-object -w LICENSE
   }
 
 }
