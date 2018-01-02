b55f5492dd0dcee821c2fef4a44148c536d6e7f8
Maxim Sukharev <maxim@sourced.tech>
2018-01-02 17:52:29 +0100

    Use engine 0.3.1
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- src/main/scala/tech/sourced/gemini/HashSparkApp.scala
+++ src/main/scala/tech/sourced/gemini/HashSparkApp.scala
@@ -13,12 +13,15 @@ case class HashAppConfig(reposPath: String = "",
                          limit: Int = 0,
                          host: String = Gemini.defaultCassandraHost,
                          port: Int = Gemini.defaultCassandraPort,
+                         format: String = "siva",
                          verbose: Boolean = false)
 
 /**
   * Apache Spark app that applied LSH to given repos, using source{d} Engine.
   */
 object HashSparkApp extends App with Logging {
+  val repoFormats = Seq("siva", "bare", "standard")
+
   val parser = new scopt.OptionParser[HashAppConfig]("./hash") {
     head("Gemini Hasher")
     note("Hashes given set of Git repositories, either from FS or as .siva files.")
@@ -32,6 +35,14 @@ object HashSparkApp extends App with Logging {
     opt[Int]('l', "limit")
       .action((x, c) => c.copy(limit = x))
       .text("limit on the number of processed repositories")
+    opt[String]('f', "format")
+      .valueName(repoFormats.mkString(" | "))
+      .withFallback(() => "siva")
+      .validate(x =>
+        if (repoFormats contains x) success else failure(s"format must be one of " + repoFormats.mkString(" | "))
+      )
+      .action((x, c) => c.copy(format = x))
+      .text("format of the stored repositories")
     opt[Unit]('v', "verbose")
       .action((_, c) => c.copy(verbose = true))
       .text("producing more verbose debug output")
@@ -62,7 +73,7 @@ object HashSparkApp extends App with Logging {
       CassandraConnector(spark.sparkContext).withSessionDo { cassandra =>
         gemini.applySchema(cassandra)
       }
-      val filesToWrite = gemini.hash(reposPath, config.limit)
+      val filesToWrite = gemini.hash(reposPath, config.limit, config.format)
       gemini.save(filesToWrite)
       println("Done")
 
