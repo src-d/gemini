9c3b4a28bd59526cc50164b467e75ceced403fc3
Maxim Sukharev <maxim@sourced.tech>
2018-03-15 11:44:34 +0100

    Create python feature extractions server
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

---
+++ src/main/python/server.py
@@ -0,0 +1,42 @@
+# make pb resolvable
+import sys
+sys.path.append('./pb')
+
+from concurrent import futures
+import argparse
+import time
+
+import grpc
+
+import pb.service_pb2 as service_pb2
+import pb.service_pb2_grpc as service_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class Service(service_pb2_grpc.FeatureExtractorServicer):
+    pass
+
+
+def serve(port):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    service_pb2_grpc.add_FeatureExtractorServicer_to_server(Service(), server)
+    server.add_insecure_port('[::]:%d' % port)
+    server.start()
+
+    # since server.start() will not block,
+    # a sleep-loop is added to keep alive
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Feature Extractor Service.')
+    parser.add_argument("--port", type=int, default=9001,
+                        help="server listen port")
+    args = parser.parse_args()
+
+    serve(args.port)
