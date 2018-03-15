c445b338dc773c874272cd0900df942fb61e200d
Maxim Sukharev <maxim@sourced.tech>
2018-03-15 14:52:57 +0100

    Add scala client generation in generate_from_proto.sh
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- src/main/resources/generate_from_proto.sh
+++ src/main/resources/generate_from_proto.sh
@@ -4,8 +4,23 @@
 
 set -e
 
-python3 -m grpc_tools.protoc -I../proto \
---python_out=../python/pb --grpc_python_out=../python/pb \
-../proto/service.proto \
-../proto/github.com/gogo/protobuf/gogoproto/gogo.proto \
-../proto/gopkg.in/bblfsh/sdk.v1/uast/generated.proto
+# Python server
+
+pip3 install grpcio==1.10.0 grpcio-tools==1.10.0
+python3 -m grpc_tools.protoc -Isrc/main/proto \
+--python_out=src/main/python/pb --grpc_python_out=src/main/python/pb \
+src/main/proto/service.proto \
+src/main/proto/github.com/gogo/protobuf/gogoproto/gogo.proto \
+src/main/proto/gopkg.in/bblfsh/sdk.v1/uast/generated.proto
+
+# Scala client
+
+wget https://github.com/scalapb/ScalaPB/releases/download/v0.7.1/scalapbc-0.7.1.zip
+unzip scalapbc-0.7.1.zip
+scalapbc-0.7.1/bin/scalapbc -Isrc/main/proto \
+--scala_out=flat_package,grpc:src/main/scala \
+src/main/proto/service.proto \
+src/main/proto/github.com/gogo/protobuf/gogoproto/gogo.proto \
+src/main/proto/gopkg.in/bblfsh/sdk.v1/uast/generated.proto
+rm scalapbc-0.7.1.zip
+rm -rf scalapbc-0.7.1
