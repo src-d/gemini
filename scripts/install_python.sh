46db71bd7bc4095cff26a1413edfe5bc64ef8adb
Alexander Bezzubov <bzz@apache.org>
2018-06-15 18:21:56 +0200

    Mute pip3 install logs on CI
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -7,4 +7,4 @@ sudo apt-get -qq update
 sudo apt-get --no-install-recommends -y install python3-pip libsnappy1 libsnappy-dev
 # pip==9.0.1 same version as sourced-ml uses
 sudo pip3 install --upgrade pip==9.0.1 setuptools wheel
-pip3 install -I --user --only-binary=numpy,scipy -r src/main/python/community-detector/requirements.txt pytest
+pip3 install -q -I --user --only-binary=numpy,scipy -r src/main/python/community-detector/requirements.txt pytest
