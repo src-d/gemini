8569f9bfbc65f44d6388b9c3e6f2c52dd2fe5926
Alexander Bezzubov <bzz@apache.org>
2018-10-01 19:16:43 +0200

    py: bump grpcio version
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -6,7 +6,5 @@ set -x
 
 sudo apt-get -qq update
 # libsnappy is needed for sourced-ml
-sudo apt-get --no-install-recommends -y install python3-pip libsnappy1 libsnappy-dev
-# pip==9.0.1 same version as sourced-ml uses
-sudo pip3 install --upgrade pip==9.0.1 setuptools wheel
+sudo apt-get --no-install-recommends -y install libsnappy1 libsnappy-dev
 pip3 install -q -I --user --only-binary=numpy,scipy -r src/main/python/community-detector/requirements.txt pytest
