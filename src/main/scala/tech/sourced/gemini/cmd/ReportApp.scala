4bf3abafe16f50aa066230d87361cae7d5497a18
Maxim Sukharev <max@smacker.ru>
2019-01-22 15:54:05 +0100

    add json output to report command
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- src/main/scala/tech/sourced/gemini/cmd/ReportApp.scala
+++ src/main/scala/tech/sourced/gemini/cmd/ReportApp.scala
@@ -1,6 +1,9 @@
 package tech.sourced.gemini.cmd
 
 import com.datastax.driver.core.Cluster
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import tech.sourced.gemini._
 import tech.sourced.gemini.util.Logger
 
@@ -12,10 +15,16 @@ case class ReportAppConfig(
   format: String = ReportApp.defaultFmt,
   ccDirPath: String = ".",
   verbose: Boolean = false,
-  mode: String = Gemini.fileSimilarityMode
+  mode: String = Gemini.fileSimilarityMode,
+  output: String = ReportApp.defaultOutput
 )
 
 object ReportApp extends App {
+  val outputText = "text"
+  val outputJson = "json"
+  val outputs = Array(outputText, outputJson)
+  val defaultOutput = outputText
+
   val defaultFmt = ""
   val defaultFmtGroupBy = "use-group-by"
   val condensedFmt = "condensed"
@@ -40,6 +49,16 @@ object ReportApp extends App {
     opt[Unit]('v', "verbose")
       .action((_, c) => c.copy(verbose = true))
       .text("producing more verbose debug output")
+    opt[String]("output")
+      .valueName(outputs.mkString(" | "))
+      .validate(x =>
+        if (outputs contains x) {
+          success
+        } else {
+          failure(s"output must be one of: " + outputs.mkString(" | "))
+        })
+      .action((x, c) => c.copy(output = x))
+      .text("output format")
     opt[String]("format")
       .valueName("use-group-by or condensed")
       .action((x, c) => c.copy(format = x))
@@ -64,10 +83,12 @@ object ReportApp extends App {
       log.info("Checking DB schema")
       gemini.applySchema(cassandra)
 
-      val ReportResult(duplicates, similarities) = gemini.report(cassandra, config.format, config.ccDirPath)
+      val result = gemini.report(cassandra, config.format, config.ccDirPath)
 
-      print(duplicates)
-      printCommunities(similarities)
+      config.output match {
+        case `outputText` => printAsText(result)
+        case `outputJson` => printAsJson(result)
+      }
 
       log.info("Closing DB connection")
       cassandra.close()
@@ -77,8 +98,10 @@ object ReportApp extends App {
       System.exit(2)
   }
 
-  def print(report: ReportDuplicates): Unit = {
-    report match {
+  def printAsText(result: ReportResult): Unit = {
+    val ReportResult(duplicates, similarities) = result
+
+    duplicates match {
       case e if e.empty() => println(s"No duplicated files found.")
       case ReportGrouped(v) => println(s"Duplicated files found:\n\t" + (v mkString "\n\t"))
       case ReportExpandedGroup(v) =>
@@ -87,13 +110,11 @@ object ReportApp extends App {
           println(s"$count duplicates:\n\t" + (item mkString "\n\t") + "\n")
         }
     }
-  }
 
-  def printCommunities(report: Iterable[Iterable[SimilarItem]]): Unit = {
-    if (report.isEmpty) {
+    if (similarities.isEmpty) {
       println(s"No similarities found.")
     } else {
-      report.foreach { community =>
+      similarities.foreach { community =>
         val count = community.size
         val typeName = community.head match {
           case SimilarFunc(_, _, _) => "functions"
@@ -103,4 +124,18 @@ object ReportApp extends App {
       }
     }
   }
+
+  def printAsJson(result: ReportResult): Unit = {
+    val ReportResult(duplicates, similarities) = result
+
+    val mapper = new ObjectMapper() with ScalaObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+
+    val str = mapper.writeValueAsString(Map(
+      "duplicates" -> duplicates,
+      "similarities" -> similarities
+    ))
+    println(str)
+  }
+
 }
